fc8741a5559f7a1d3068f5f38cb24905d2e3d2f2
brunoruy7 <brunoruy79@gmail.com>
2022-10-14 16:01:36 -0300

    adicionado pagina inicial, modelo, header e footer

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+# def index(request):
+#     return redirect('/agenda/')
+
+def eventos(request, nome_evento):
+    event = Evento.objects.get(titulo=nome_evento)
+    local = event.local
+    return HttpResponse('Seu evento será em {}.'.format(local))
+
+def listaeventos(request):
+    evento = Evento.objects.all()
+    response = {'eventos':evento}
+    return render(request, 'Agenda.html', response)
